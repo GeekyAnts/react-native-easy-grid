4255582c4c4b2337eb08bfec20ade17caab4b046
William Candillon <w@28.io>
2016-11-16 10:53:53 +0100

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+declare module "react-native-easy-grid" {
+
+    import React, { Component } from "react";
+    import {ViewProperties} from "react-native";
+
+    export interface GridProps extends ViewProperties {}
+
+    export interface RowProps extends ViewProperties {
+        size?: number
+    }
+
+    export interface ColProps extends ViewProperties {
+        size?: number
+    }
+
+    export class Grid extends Component<GridProps, any> {}
+    export class Row extends Component<RowProps, any> {}
+    export class Col extends Component<ColProps, any> {}
+}

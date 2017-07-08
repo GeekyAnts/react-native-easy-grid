43fd6ff49788a27ddc28e4bf364f28cd39c18e46
Sankhadeep <sankhadeep@sahusoft.com>
2017-07-08 16:08:39 +0530

    fix for rn .46

--- Utils/computeProps.js
+++ Utils/computeProps.js
@@ -1,56 +1,49 @@
-var React = require('react');
-import ReactNativePropRegistry from 'react-native/Libraries/Renderer/src/renderers/native/ReactNativePropRegistry';
-var _ = require('lodash');
+var React = require("react");
+import ReactNativePropRegistry
+  from "react-native/Libraries/Renderer/shims/ReactNativePropRegistry";
+var _ = require("lodash");
 
 module.exports = function(incomingProps, defaultProps) {
+  // External props has a higher precedence
+  var computedProps = {};
 
-    // External props has a higher precedence
-    var computedProps = {};
+  incomingProps = _.clone(incomingProps);
+  delete incomingProps.children;
 
-    incomingProps = _.clone(incomingProps);
-    delete incomingProps.children;
+  var incomingPropsStyle = incomingProps.style;
+  delete incomingProps.style;
 
-    var incomingPropsStyle = incomingProps.style;
-    delete incomingProps.style;
+  // console.log(defaultProps, incomingProps);
 
-    // console.log(defaultProps, incomingProps);
+  if (incomingProps) _.merge(computedProps, defaultProps, incomingProps);
+  else computedProps = defaultProps;
 
-    if(incomingProps)
-        _.merge(computedProps, defaultProps, incomingProps);
-    else
-        computedProps = defaultProps;
-
-    // Pass the merged Style Object instead
-    if(incomingPropsStyle) {
-
-        var computedPropsStyle = {};
-        computedProps.style = {};
-        if (Array.isArray(incomingPropsStyle)) {
-            _.forEach(incomingPropsStyle, (style)=>{
-                if(typeof style == 'number') {
-                    _.merge(computedPropsStyle, ReactNativePropRegistry.getByID(style));
-                } else {
-                    _.merge(computedPropsStyle, style);
-                }
-            })
-
+  // Pass the merged Style Object instead
+  if (incomingPropsStyle) {
+    var computedPropsStyle = {};
+    computedProps.style = {};
+    if (Array.isArray(incomingPropsStyle)) {
+      _.forEach(incomingPropsStyle, style => {
+        if (typeof style == "number") {
+          _.merge(computedPropsStyle, ReactNativePropRegistry.getByID(style));
+        } else {
+          _.merge(computedPropsStyle, style);
         }
-        else {
-            if(typeof incomingPropsStyle == 'number') {
-                computedPropsStyle = ReactNativePropRegistry.getByID(incomingPropsStyle);
-            } else {
-                computedPropsStyle = incomingPropsStyle;
-            }
-        }
-
-        _.merge(computedProps.style, defaultProps.style, computedPropsStyle);
-
-
+      });
+    } else {
+      if (typeof incomingPropsStyle == "number") {
+        computedPropsStyle = ReactNativePropRegistry.getByID(
+          incomingPropsStyle
+        );
+      } else {
+        computedPropsStyle = incomingPropsStyle;
+      }
     }
 
-    // console.log("computedProps ", computedProps);
+    _.merge(computedProps.style, defaultProps.style, computedPropsStyle);
+  }
 
-    return computedProps;
+  // console.log("computedProps ", computedProps);
 
-
-}
+  return computedProps;
+};
